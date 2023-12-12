05389d736344aa768117436946ff58892855ea3a
ZfauX <153195067+ZfauX@users.noreply.github.com>
2023-12-12 15:33:42 +0300

    Delete terraform/output.tf

--- terraform/output.tf
+++
@@ -1,6 +0,0 @@
-output "instance_external_ip_app" {
-  value = yandex_compute_instance_group.app.instances.*.network_interface.0.nat_ip_address
-}
-output "instance_external_ip_db" {
-  value = yandex_compute_instance_group.db.instances.*.network_interface.0.nat_ip_address
-}
