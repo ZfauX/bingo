afc4fd8bb4823b443f085432eda4febef4f031d8
ZfauX <153195067+ZfauX@users.noreply.github.com>
2023-12-12 15:33:30 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -188,3 +188,9 @@ resource "yandex_lb_network_load_balancer" "default" {
     }
   }
 }
+output "instance_external_ip_app" {
+  value = yandex_compute_instance_group.app.instances.*.network_interface.0.nat_ip_address
+}
+output "instance_external_ip_db" {
+  value = yandex_compute_instance_group.db.instances.*.network_interface.0.nat_ip_address
+}
